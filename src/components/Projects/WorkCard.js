d02d132f6e4da10fa26adf68f9be95e72ad15656
Dmitriy <93647407+1swaN@users.noreply.github.com>
2024-01-31 19:29:49 +0400

    some visual fixes in workcard

--- src/components/Projects/WorkCard.js
+++ src/components/Projects/WorkCard.js
@@ -9,17 +9,19 @@ let WorkCard = () => {
   return (
     <div className="work__card element-animation">
       <Image />
-      <div className="card__info" href="https://penduloconelena.com/" target="_blank">
-        <a className="card__header" href="https://penduloconelena.com/" target="_blank">Why psychotherapy matters?</a>
-        <p className="card__text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sunt
-          obcaecati voluptas, accusamus, dolore velit rerum porro debitis eum,
-          consectetur aliquam dicta facilis recusandae quas voluptatum pariatur
-          vero assumenda quidem quibusdam ipsam. Adipisci sapiente placeat
-          labore quo pariatur, voluptatem voluptates? Vero unde, voluptatem quod
-          et dolor placeat illum atque assumenda!
-        </p>
-      </div>
+        <div className="card__info">
+          <div class="link-ease-in-out">
+            <a className="card__header" href="https://penduloconelena.com/" target="_blank">Why psychotherapy matters?</a>
+          </div>
+          <p className="card__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sunt
+            obcaecati voluptas, accusamus, dolore velit rerum porro debitis eum,
+            consectetur aliquam dicta facilis recusandae quas voluptatum pariatur
+            vero assumenda quidem quibusdam ipsam. Adipisci sapiente placeat
+            labore quo pariatur, voluptatem voluptates? Vero unde, voluptatem quod
+            et dolor placeat illum atque assumenda
+          </p>
+        </div>
     </div>
   );
 };
